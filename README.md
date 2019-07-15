6aecab595d7ba24f8a3ea7b587e59e524fd85681
cmoyano <cristianmoyano.mza@gmail.com>
2019-07-15 01:57:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Docker image and python sever in flask with ALPR( Automatic License Plate Recognition ) with the bindings for python.
+
+To develop
+```bash
+make dev
+```
+
+To build the image and run it
+```bash
+make run-prod
+```
+
+To test it
+```bash
+make test-prod
+```
---
+++ server.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import uuid
+
+from flask import Flask, request, abort, flash, jsonify
+from openalpr import Alpr
+
+app = Flask(__name__)
+
+alpr = Alpr("eu", "/openalpr/config/alprd.conf.defaults", "/openalpr/runtime_data")
+
+if not alpr.is_loaded():
+    print("Error loading OpenALPR")
+    sys.exit(1)
+
+alpr.set_top_n(20)
+alpr.set_default_region("md")
+
+
+@app.route('/', methods=['POST'])
+def get_plate_number():
+    filename = str(uuid.uuid4())
+    path_join = os.path.join("/tmp", filename)
+
+    multipart_file = request.files['']
+    multipart_file.save(path_join)
+    results = alpr.recognize_file(path_join)
+    os.remove(path_join)
+    return jsonify(results['results'])
+
+
+if __name__ == '__main__':
+    	app.secret_key = 'super secret key'
+	app.run(debug=True, host='0.0.0.0')
